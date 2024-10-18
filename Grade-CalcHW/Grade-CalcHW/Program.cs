3cbff1f00becdb9915f96a66a86b44beccbcb0cf
Tommy Seternus <thomassseternus@gmail.com>
2024-10-18 17:05:44 -0400

    Initial Commit

---
+++ Grade-CalcHW/Grade-CalcHW/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Grade_CalcHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName;
+
+            studentName = "Tommy";
+            Console.WriteLine("Hello, " + studentName);
+        }
+    }
+}
